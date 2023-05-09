745769bb2ed06b208fc32499f6cd4519591ddd6f
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-05-09 17:28:29 +0200

    Add files via upload

---
+++ Coin_Flip_Streaks.py
@@ -0,0 +1,22 @@
+import random
+numberOfStreaks = 0
+result = []
+headsCounter = 0
+tailsCounter = 0
+for experimentNumber in range(10000):
+    # Code that creates a list of 100 'heads' or 'tails' values.
+    headsOrTails = random.randint(0, 1)
+    result.append(headsOrTails)
+    if headsOrTails == 0:
+        headsCounter += 1
+        tailsCounter = 0
+        if headsCounter == 6:
+            numberOfStreaks += 1
+    else:
+        tailsCounter += 1
+        headsCounter = 0
+        if tailsCounter == 6:
+            numberOfStreaks += 1
+    # I can make a checker variable for both
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
